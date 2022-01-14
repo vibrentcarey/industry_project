ebb4620c8c3828bfda2d4141b5d81ae5098e19ec
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-13 23:01:02 -0800

    add form

--- components/AvatarCard.js
+++ components/AvatarCard.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 
-const AvatarCard = ({ fname, lname, title,  }) => {
+const AvatarCard = ({ fname, lname, title, }) => {
+  // Mode State
+  const [editMode, setEditMode] = useState(false)
+
+  // Form State
   const [form, setForm] = useState({
     fullName: 'Willy Solomons',
     companyName: 'Eggs and Flour',
     about: 'Enter a little something about your business to tell customers about yourself',
     email: 'eggsandflour@gmail.com',
     phone: '604 975 8463',
-    address: '2173 W 41 Avenue, Vancouver, BC'
+    address: '2173 W 41 Avenue, Vancouver, BC',
   })
+
+  const handleEdit = () => {
+    setEditMode(!editMode)
+  }
+
+  const handleAbout = (e) => {
+    setForm(prevForm => {
+      return {
+        ...prevForm,
+        about: e.target.value
+      }
+    })
+  }
   return (
-    <div className='w-full bg-backgroundGray p-6 rounded-md mt-20 flex flex-col items-center shadow-lg'>
-      <span className='self-end text-xs mx-1 text-primaryBlue'>edit</span>
+    <div className='w-full bg-backgroundGray p-6 rounded-md mt-10 mb-10 flex flex-col items-center shadow-lg'>
+      <span className='self-end text-xs mx-1 text-primaryBlue' onClick={handleEdit}>{editMode ?'save' : 'edit'}</span>
       {/* Profile Info */}
       <article className='w-1/2 flex flex-col items-center'>
         <img className='' src='./profile-image.png' />
@@ -25,27 +42,32 @@ const AvatarCard = ({ fname, lname, title,  }) => {
         </div>
         {/* Business Info */}
       </article>
-      <h1 className='m-2 text-xl font-bold'>{title}</h1>
-      <p className='text-sm w-11/12 font-light'>{form.about}</p>
+      {editMode || <div className='flex flex-col items-center mt-4'>
+        <h1 className='m-2 text-xl font-bold'>{title}</h1>
+        <p className='text-sm w-11/12 font-light'>{form.about}</p>
+      </div>}
 
       {/* Form */}
-      <form className='flex flex-col w-full mt-4'>
-      {/* Name */}
-        <label className='text-sm font-light'>Your Name</label>
-        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.fullName}/>
+      {editMode && <form className='flex flex-col w-full mt-4'>
+        {/* Name */}
+        <label className='text-sm font-light' readOnly>Your Name</label>
+        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.fullName} />
         {/* Company */}
-        <label className='text-sm font-light mt-4'>Company Name</label>
-        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.companyName}/>
+        <label className='text-sm font-light mt-4 ' readOnly>Company Name</label>
+        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.companyName} />
         {/* Description */}
         <label className='text-sm font-light mt-4'>About</label>
-        <textarea className='mt-1 border-2 rounded-md h-28 p-2 text-sm border-black bg-backgroundGray' value={form.fullName}/>
+        <textarea className='mt-1 border-2 rounded-md h-28 p-2 text-sm border-black bg-backgroundGray' placeholder={form.about} onChange={handleAbout} />
         {/* Email */}
-        <label className='text-sm font-light mt-4'>Email</label>
-        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.email}/>
+        <label className='text-sm font-light mt-4' readOnly>Email</label>
+        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.email} />
         {/* Phone */}
-        <label className='text-sm font-light mt-4'>Phone</label>
-        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.phone}/>
-      </form>
+        <label className='text-sm font-light mt-4' readOnly>Phone</label>
+        <input className='mt-1 border-b-2 border-black bg-backgroundGray' value={form.phone} />
+        {/* Address */}
+        <label className='text-sm font-light mt-4' readOnly>Address</label>
+        <input className='mt-1 mb-6 border-b-2 border-black bg-backgroundGray' value={form.address} />
+      </form>}
     </div>
   );
 };
