8d16cdcc8953e463478620e44a2245850b1f9bb7
lockedNLevered <shaynel@sfu.ca>
2022-01-14 10:13:35 -0800

    move top bar overlap nav

--- components/TopBar.js
+++ components/TopBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const TopBar = ({ ...props }) => {
 	return (
 		<div
-			className={`w-full bg-primaryBlack flex justify-around py-2 rounded-full shadow-xl ${props.className}`}
+			className={`w-full bg-primaryBlack flex justify-around py-2 rounded-full shadow-xl -mt-14 ${props.className}`}
 		>
 			<h2 className="text-white">Profile</h2>
 		</div>
---
+++ pages/redemption.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PageWrapper from "../components/PageWrapper";
+import TopBar from "../components/TopBar"
+export default function Redemption() {
+	return <PageWrapper><TopBar /></PageWrapper>
+}
