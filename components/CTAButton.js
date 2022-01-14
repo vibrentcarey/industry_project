01bba3262e6af367ccc16142a6529c85907e703a
lockedNLevered <shaynel@sfu.ca>
2022-01-14 10:58:23 -0800

    top bar changes title based onroute

--- components/CTAButton.js
+++ components/CTAButton.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+
+
+
 export default function CTAButton({ add, yellow, children, primary, ...props }) {
 	return (
 		<button
--- pages/redemption.tsx
+++ pages/redemption.tsx
@@ -15,9 +15,15 @@ export default function Redemption({}: Props) {
 			<TopBar />
 			<div className="flex flex-col gap-4">
 				<ProductRemptionCard name="Cupcake" image="./Cupcake.png" points={10} />
-				<ProductRemptionCard name="High Fibre Bread" image="./Cookie.png" points={20} />
+				<ProductRemptionCard
+					name="High Fibre Bread"
+					image="./Cookie.png"
+					points={20}
+				/>
 				<ProductRemptionCard name="Cake" image="./Cake.png" points={30} />
-                <CTAButton primary add >Add Item</CTAButton>
+				<CTAButton primary add>
+					Add Item
+				</CTAButton>
 			</div>
 		</PageWrapper>
 	);
