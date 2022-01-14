26b36c962e120e7fc345f65116a58d36c5e34f0e
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-14 13:16:38 -0800

    Final commit.. almost

--- components/PageWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function PageWrapper({ children }) {
-	return (
-		<main className="min-h-screen bg-backgroundGray rounded-2xl px-10 py-10 flex flex-col gap-4 pb-16">{children}</main>
-	);
-}
--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-interface Props {
-	children: React.ReactNode;
-}
-
-export default function PageWrapper({ children }: Props) {
+export default function PageWrapper({ children }) {
 	return (
-		<main className="h-screen bg-backgroundGray rounded-2xl px-8 py-10 flex flex-col gap-4">
-			{children}
-		</main>
+		<main className="min-h-screen bg-backgroundGray rounded-2xl px-10 py-10 flex flex-col gap-4 pb-16">{children}</main>
 	);
 }
