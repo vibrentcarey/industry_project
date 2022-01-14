f25fb68718c7c631de09cbb08275b84e1943d359
lockedNLevered <shaynel@sfu.ca>
2022-01-13 22:39:42 -0800

    add all cards to program option page

--- components/LoyaltyCard.js
+++ components/LoyaltyCard.js
@@ -1,11 +1,22 @@
 import React from "react";
 import CreateButton from "./CreateButton";
 
-export default function LoyaltyCard({ title, message }) {
-	return (
-		<div className="flex flex-col justify-between w-[47%] gap-4 shadow-xl rounded-lg p-4">
+
+
+export default function LoyaltyCard({ title, message, premium, icon }) {
+	return premium ? (
+		<div className="flex flex-col justify-between w-[47%] gap-4 shadow-xl rounded-lg p-4 ">
 			<h1 className="text-xl font-extrabold">{title}</h1>
-			<p>{message}</p>
+			<p className="text-xs">{message}</p>
+			<div className="flex justify-between">
+				<CreateButton />
+				<img className="h-10" src={icon} />
+			</div>
+		</div>
+	) : (
+		<div className="flex flex-col justify-between w-[47%] gap-4 shadow-xl rounded-lg p-4 bg-white">
+			<h1 className="text-xl font-extrabold">{title}</h1>
+			<p className="text-xs">{message}</p>
 			<CreateButton />
 		</div>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -48,29 +48,7 @@ export default function Home() {
 				/>
 			</div>
 			<PremiumCard>
-        <h1>
-          PREMIUM
-        </h1>
-				<div className="flex flex-wrap gap-2 justify-around">
-					<PromoBadge icon="./bi_currency-dollar.svg" title="Monitor Revenue" />
-					<PromoBadge icon="./fa-solid_users.svg" title="Track Loyal Users" />
-					<PromoBadge
-						icon="./bx_bx-wifi.svg"
-						title="Increase Your Online Presence"
-					/>
-					<PromoBadge
-						icon="./healthicons_medical-advice.svg"
-						title="Personal Business Advisor"
-					/>
-					<PromoBadge
-						icon="./fluent_arrow-trending-lines-20-filled.svg"
-						title="Current Business Trends"
-					/>
-					<PromoBadge
-						icon="./ep_data-analysis.svg"
-						title="Market Analysis"
-					/>
-				</div>
+       
 			</PremiumCard>
 		</PageWrapper>
 	);
